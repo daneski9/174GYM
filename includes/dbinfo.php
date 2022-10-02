57579b4dfc3192d3db55a3dc30e24a9925618b1d
Dane Coleman <danecoleman@csus.edu>
2022-10-02 01:24:34 -0700

    includes file, db info moved

--- includes/dbinfo.php
+++ includes/dbinfo.php
@@ -4,4 +4,4 @@ $db_user = "b800f6d94d478e";
 $db_pass = "40f775a2";
 $db_name = "heroku_e56a44749efb109";
 
-$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-Test
+<?php 
+    include_once 'includes/includes.php'; #path to the file 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GYM MANAGEMENT DATABASE</title>
+</head>
+<body>
+    <?php
+    $connect;
+    if(mysqli_connect_errno()){
+    echo "Failed to connect!";
+    exit();
+    }
+
+    echo "Connection succes!";
+    $sql = "SELECT gymID, email FROM gym";
+    $result = $connect->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+        echo "gymID: " . $row["gymID"]. " - email: " . $row["email"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+   $connect->close();
+   ?>
+
+</body>
+</html>
