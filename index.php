f23665763cc39ba745e18fde24ae66b2a3559d86
Dane Coleman <danecoleman@csus.edu>
2022-11-22 23:10:03 -0800

    .

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 
 <?php
-// LOCAL TEST SERVER
-$servername = "localhost";
-$username = "root";
-$password = "dane";
-$db_name = "gymproject";
-// Create connection
-$connect = new mysqli($servername, $username, $password, $db_name);
-// Check connection
-if ($connect->connect_error) {
-  die("Connection failed: " . $connect->connect_error);
-}
-echo "Connected successfully";
-#include_once 'includes/dbinfo.php'; #path to the file 
+include_once 'includes/dbinfo.php'; #path to the file 
 ?>
 <!DOCTYPE html>
 <html lang="en">
